936229842b3d3ec8ed97b2c3268372e37b127c9a
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-16 23:50:39 -0300

    feature(Flat): Flat entity

---
+++ src/database/entity/Flat.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export default class Flat {
+
+  @PrimaryGeneratedColumn("increment")
+  id!: number;
+  
+  @Column("varchar", {
+    length: 45,
+    nullable: false,
+    unique: true
+  })
+  name!: string;
+  
+  @Column("decimal", {
+    precision: 5,
+    scale: 2,
+    nullable: false,
+    unique: true
+  })
+  price!: number;
+}
