54226f05c9464175128ba220199c626162fc561e
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-17 23:44:05 -0300

    feat(Account): Account crud and login routes

--- src/router/account.routes.ts
+++ src/router/account.routes.ts
@@ -4,8 +4,9 @@ import AccountController from "../controller/AccountController";
 const routes = Router();
 
 routes.get(`/`, AccountController.findAll);
-// routes.post(`/`, AccountController.create);
-// routes.put(`/`, AccountController.update);
-// routes.delete(`/:id`, AccountController.delete);
+routes.post(`/`, AccountController.create);
+routes.put(`/`, AccountController.update);
+routes.delete(`/`, AccountController.delete);
+routes.post(`/login`, AccountController.login);
 
 module.exports = routes;
