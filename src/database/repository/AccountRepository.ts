b72db5ae9fa304de1514a1fa80ac773282d2f241
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-17 23:43:56 -0300

    feat(Account): Account crud and login repository

--- src/database/repository/AccountRepository.ts
+++ src/database/repository/AccountRepository.ts
@@ -1,9 +1,26 @@
 import { EntityRepository, Repository } from "typeorm";
 import Account from "../entity/Account";
+import Flat from "../entity/Flat";
 
 @EntityRepository(Account)
 export default class AccountRepository extends Repository<Account> {
   findAll(): Promise<Account[]> {
     return this.find({ select: ["id", "email", "flat"], relations: ["flat"] });
+  }
+
+  createAndSave(email: string, password: string, flat: Flat): void {
+    this.save({ email, password, flat });
+  }
+
+  updateById(id: number, email: string, flat: Flat): void {
+    this.save({ id, email, flat });
+  }
+
+  deleteById(id: number): void {
+    this.delete({ id });
+  }
+
+  login(email: string, password: string): Promise<Account | undefined> {
+    return this.findOne({ select: ["id", "email", "flat"], relations: ["flat"], where: { email, password } });
   }
 }
