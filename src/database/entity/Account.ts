cbda6bbc4eea8c3db400a17d7d041b0870bf8fc3
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-18 21:55:52 -0300

    feat(Account): encrypt password

--- src/database/entity/Account.ts
+++ src/database/entity/Account.ts
@@ -1,26 +1,38 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  BeforeInsert,
+  BeforeUpdate
+} from "typeorm";
 import Flat from "./Flat";
-
+import bcrypt from "bcryptjs";
 @Entity()
 export default class Account {
-  
   @PrimaryGeneratedColumn("increment")
   id!: number;
 
   @Column("varchar", {
     length: 100,
     nullable: false,
-    unique: true
+    unique: true,
   })
   email!: string;
 
   @Column("varchar", {
     length: 45,
     nullable: false,
-    unique: true
+    unique: true,
   })
   password!: string;
 
-  @ManyToOne(() => Flat, flat => flat.id)
-  flat!: Flat
+  @ManyToOne(() => Flat, (flat) => flat.id)
+  flat!: Flat;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
 }
