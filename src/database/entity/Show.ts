f2e4f0cd9b48e3a5bcc1737e04aa5ad01cea3062
custodiomatheus <matheuscustodio76@gmail.com>
2021-09-19 20:05:59 -0300

    feat(Show): added show type

--- src/database/entity/Show.ts
+++ src/database/entity/Show.ts
@@ -1,10 +1,15 @@
-import { Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import UsersShows from "./UsersShows";
 
 @Entity({ name: "shows" })
 export default class Show {
   @PrimaryGeneratedColumn("increment")
   id!: number;
+
+  @Column("varchar", {
+    nullable: false
+  })
+  type!: string
 
   @OneToMany(() => UsersShows, (userShow) => userShow.show, {
     cascade: true,
